16c0bd21cb54861844dc30d77a16766e63b2aedc
Roneesh <roneesh@gmail.com>
2014-08-10 18:05:39 -0500

    calendar showing all days for august

--- calendaria.js
+++ calendaria.js
@@ -3,12 +3,19 @@ $(document).ready(function() {
 	"use strict";
 
 	// 5 july days, 31 August days, 6 September days
-	var August = [5, 31, 6];
+	var August = [[5,31], 31, 6];
 
-	for(var i = 0; i < August[0]; i++) {
-		$('.calendar-area').append("<div class='day prior-month-day'></div>");
+	for(var i = (August[0][0]-1); i >= 0; i--) {
+		// console.log(i);
+		var date = August[0][1] - (i);
+		var dateToString = date.toString();
+		console.log(dateToString);
+		var numericalDate = "<div class='numericalDate'>" + dateToString  + "</div>";
+		var day = "<div class='day prior-month-day'>" + numericalDate + "</div>";
+		$('.calendar-area').append(day);
 
 	}
+
 	for (var i = 0; i < August[1]; i++) {
 		var dateToString = (i+1).toString();
 		var numericalDate = "<div class='numericalDate'>" + dateToString  + "</div>";
@@ -16,14 +23,14 @@ $(document).ready(function() {
 		$('.calendar-area').append(day);
 	}
 	for (var i = 0; i < August[2]; i++) {
-		$('.calendar-area').append('<div class="day post-month-day"></div>');
+		var dateToString = (i+1).toString();
+		var numericalDate = "<div class='numericalDate'>" + dateToString  + "</div>";
+		var day = "<div class='day post-month-day'>" + numericalDate + "</div>";
+		$('.calendar-area').append(day);
 	}
 
-	$.each($('.day'), function() {
-		var numericalDate = $(this).data();
-		// console.log(this);
-		// console.log($(this));
-	})
+
+
  
 
 });
